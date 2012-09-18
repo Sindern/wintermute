63d77858bb0ea9055c1b10c0fdf2b6cad97407ce
Sindern <sindern@hotmail.com>
2012-09-18 04:52:15 -0500

    fixin' a sudo error

--- speech/shutup.sh
+++ speech/shutup.sh
@@ -1,4 +1,4 @@
 ## Kill all open festival and mplayer processes
-sudo killall aplay ## shuts wintermute the hell up if she's giving info.
-sudo killall mplayer ## stop playback of any media files.
+killall aplay ## shuts wintermute the hell up if she's giving info.
+killall mplayer ## stop playback of any media files.
 echo "I'm shutting the hell up now."
