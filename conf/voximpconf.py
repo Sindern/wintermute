b1f4b8a414613532342ba30e3a45b3e1b9c25231
Sindern <sindern@hotmail.com>
2012-09-18 07:02:21 -0500

    fixed a problem with colored text not displaying in the weather script.

--- conf/voximpconf.py
+++ conf/voximpconf.py
@@ -22,11 +22,11 @@ keycommand = {
 
 programcommand = {
   'WINTERMUTE': "touch $HOME/wintermute/wmlisten ; sleep 5; rm -f $HOME/wintermute/wmlisten",
-	'WEATHER': "/home/wintermute/Dropbox/ProjCoding/automation_bot/speech/weather.sh",
-	'DATE': "/home/wintermute/Dropbox/ProjCoding/automation_bot/speech/date.sh",
-	'TIME': "/home/wintermute/Dropbox/ProjCoding/automation_bot/speech/time.sh",
-	'SHUTUP': "/home/wintermute/Dropbox/ProjCoding/automation_bot/speech/shutup.sh",
-	'CAPTAINS LOG': "/home/wintermute/Dropbox/ProjCoding/automation_bot/speech/captainslog.sh",
+	'WEATHER': "bash /home/wintermute/Dropbox/ProjCoding/automation_bot/speech/weather.sh",
+	'DATE': "bash /home/wintermute/Dropbox/ProjCoding/automation_bot/speech/date.sh",
+	'TIME': "bash /home/wintermute/Dropbox/ProjCoding/automation_bot/speech/time.sh",
+	'SHUTUP': "bash /home/wintermute/Dropbox/ProjCoding/automation_bot/speech/shutup.sh",
+	'CAPTAINS LOG': "bash /home/wintermute/Dropbox/ProjCoding/automation_bot/speech/captainslog.sh",
 }
 
 mousecommand = {
