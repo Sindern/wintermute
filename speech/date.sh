8060562e2104f32cd505be05b08fdd71337fb8da
Sindern <sindern@hotmail.com>
2012-09-18 04:16:41 -0500

    path fix

--- speech/date.sh
+++ speech/date.sh
@@ -5,11 +5,11 @@ if [[ ! -f $HOME/wintermute/wmlisten ]]
 fi
 
 if [[ "$(echo $(date +%Y)|egrep "^20")" == "" ]]
-  then year="$(conv/num_to_txt.sh $(date +%Y))"
-else year="twenty $(conv/num_to_txt.sh $(date +%y))"
+  then year="$($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%Y))"
+else year="twenty $($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%y))"
 fi
 month="$(date +%B)"
-day="$(bash conv/num_to_ord.sh $(date +%d|sed 's|^0||g'))"
+day="$($HOME/wintermute/speech/conv/num_to_ord.sh $(date +%d|sed 's|^0||g'))"
 
 today="$month $day, $year"
 echo "Returned date: $today"
--- speech/time.sh
+++ speech/time.sh
@@ -9,13 +9,13 @@ if [[ "$(date +%H)" == "00" ]]
 elif [[ "$(date +%H)" == "12" ]]
  then hour="noon"
 elif [[ "$(date +%H)" -gt "12" ]]
- then hour="$(conv/num_to_txt.sh $(date +%I))"
+ then hour="$($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%I))"
   ampm="pee ehm"
-else hour="$(conv/num_to_txt.sh $(date +%H))"
+else hour="$($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%H))"
   ampm="ae ehm"
 fi
-minute="$(conv/num_to_txt.sh $(date +%M))"
-seconds="$(conv/num_to_txt.sh $(date +%S))"
+minute="$($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%M))"
+seconds="$($HOME/wintermute/speech/conv/num_to_txt.sh $(date +%S))"
 
 response="It is $hour $minute $ampm , $seconds seconds"
 echo "Time Query: $response"
