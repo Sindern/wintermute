bbb8968a4b17195b9f8aa0957a1b1a5c0f09872b
Sindern <sindern@hotmail.com>
2012-09-18 06:59:35 -0500

    updated readme and added a new weather script using weatherunderground's API

--- speech/weather.sh
+++ speech/weather.sh
@@ -1,19 +1,41 @@
-#! /bin/bash
+## Weather update daemon.  Update time can be adjusted to fit the API's limits. 
+updateseconds=300 # amount of time to wait between updates to stay within Weather Underground's free API limit.
 
-if [[ ! -f $HOME/wintermute/wmlisten ]]
-  then echo "Weather response triggered, but Wintermute wasn't listening." ; exit
+## Color variables
+txtgrn='\e[0;32m' # Green
+bldgrn='\e[1;32m' # Green
+bldblu='\e[1;34m' # Blue
+bldwht='\e[1;37m' # White
+txtrst='\e[0m'    # Text Reset
+
+## Grab the update file to tmp dir if it doesn't exist or it's been more than $updateseconds since last update.
+[[ ! -d $HOME/tmp/wintermute/ ]] &&  mkdir -p $HOME/tmp/wintermute/
+file="$HOME/tmp/wintermute/Austin.json"
+lastupdate=$(expr $(date +%s) - $(stat --format=%Y $file))
+if [[ $lastupdate -gt $updateseconds ]]
+ then wget -O $file http://api.wunderground.com/api/a78ff389f1302f7b/conditions/q/TX/Austin.json 2> /dev/null 1> /dev/null
+  echo -e "${bldblu}--==(Updating with the latest weather.)==--\n\t${bldwht}(last update $lastupdate sec ago) ${txtrst}"
+else echo -e "${bldblu}--==(Displaying Cached Weather Data)==--\n\t${bldwht}(last update $lastupdate sec ago) ${txtrst}"
 fi
+## Grab the information we want to display using grep with perl lookahead/lookbehinds and -o to display only what we need
+city=$(grep -Po "(?<=\"city\":\").*(?=\",)" $file | head -n 1)
+state=$(grep -Po "(?<=\"state_name\":\").*(?=\",)" $file|head -n 1)
+weather=$(grep -Po "(?<=\"weather\":\").*(?=\",)" $file|head -n 1)
+temp=$(grep -Po "(?<=\"temp_f\":).*(?=,)" $file|head -n 1)
+humidity=$(grep -Po "(?<=\"relative_humidity\":\").*(?=\",)" $file|head -n 1 | sed 's|%| percent|g')
+windstring=$(grep -Po "(?<=\"wind_string\":\").*(?=\",)" $file|head -n 1)
+winddir=$( grep -Po "(?<=\"wind_dir\":\").*(?=\",)" $file|head -n 1)
+windmph=$(grep -Po "(?<=\"wind_mph\":).*(?=,)" $file|head -n 1)
+windgustmph=$(grep -Po "(?<=\"wind_gust_mph\":).*(?=,)" $file|head -n 1)
 
-##sample output: Clear@Temperature@69.3 °F@Feels Like 69.3 °F@Wind(mph)@0.0@Sunrise / Set@7:17 AM@7:31 PM@Moon@Waxing Crescent
-wstr=$(lynx --dump http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=Austin%2C+TX |grep -v "http://" | grep -v "javascript:void" | grep -v "  \* "|sed '1,/  Now/d' | sed 1d | head -n 11 | sed 's|^\s*||g'|sed ':a;N;$!ba;s/\n/@/g'|sed 's|°F|fahrenheit|g')
-
-curcond=$(echo $wstr|cut -d'@' -f1)
-curtemp="$(echo $wstr|cut -d'@' -f3)"
-wind="$(echo $wstr|cut -d'@' -f6|cut -d'.' -f1)"
-response="Currently $curcond and $curtemp with wind at $wind miles per hour."
-echo "Weather inquiry: $response"
+#formulate a response and echo it to festival
+response="Weather is $weather in $city $state. Temperature is $temp fahrenheit with $humidity humidity.  There is a $windstring wind from the $winddir at $windmph miles per hour, gusting to $windgustmph miles per hour."
 echo $response | festival --tts 2> /dev/null &
-
-
-#Moon is a $(echo $wstr|cut -d'@' -f11)
-#feelslike="$(echo $wstr|cut -d'@' -f4|egrep -o '[0-9]+\.?'|sed 's|\.||g')"
+#and print out some pretty shit to the terminal.
+echo -e "\n\t${bldblu}Weather for $city, $state.${txtrst}"
+echo -e "${bldgrn}Weather:\t${txtgrn}$weather${txtrst}"
+echo -e "${bldgrn}Temperature:\t${txtgrn}$temp${txtrst}"
+echo -e "${bldgrn}Humidity:\t${txtgrn}$humidity${txtrst}"
+echo -e "${bldgrn}Wind Info:\t${txtgrn}$windstring from the $winddir${txtrst}"
+echo -e "${bldgrn}WindSpeed:\t${txtgrn}$windmph mph (gust $windgustmph mph)${txtrst}"
+echo
