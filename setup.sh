fcb74626db110f3091fa2e0c91f17aa0ae7de8a3
Alan <alan.congleton@wpengine.com>
2017-09-30 09:56:18 -0500

    conf tweak

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
-#Check to see if the packages we need exist.
-
+echo "Checking to see if the packages we need exist."
 for package in git motion ; do
   dpkg -l ${package} || install_packages+=" ${install_packages} "
 done
 
-#Install the packages that were missing.
 if [[ -n ${install_packages}]]; then
+  echo "Installing necessary packages."
   sudo apt-get install ${install_packages}
+else
+  echo "All necessary packages already installed."
 fi
+
+# Set up the camera
+echo "Checking for Video Device on /dev/vid*"
+ls -al /dev/vid* && Echo "Video Device Found."|| sudo modprobe bcm2835-v4l2
+ls -al /dev/vid* && echo "Video Device Found." || echo "Failed to find a video device."
