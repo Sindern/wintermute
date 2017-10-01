06ed10672b9898d8fba449d166c6ceb18a74f44f
Alan <alan.congleton@wpengine.com>
2017-10-01 06:34:11 -0500

    jpegoptim secout images to safe space/bandwidth

--- scripts/rclone_lock.sh
+++ scripts/rclone_lock.sh
@@ -4,6 +4,7 @@ lock=/tmp/rclone_running
 
 if [[ ! -f ${lock}  ]] ; then
    touch ${lock}
+   jpegoptim /home/pi/Documents/secout/*
    rclone move /home/pi/Documents/secout/ secout:/secout/
    rm ${lock}
 fi
