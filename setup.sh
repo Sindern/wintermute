9a76b0649843e2145aa5eec7a5b7a4bc7be5f224
Alan <alan.congleton@wpengine.com>
2017-10-01 06:06:02 -0500

    option to remove unnecessary junk for space.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,36 @@
 #! /bin/bash
 
+echo -e "Checking for aptitude."
+dpkg -l aptitude || { sudo apt-get update ; sudo apt-get -y install aptitude ; }
+
+sudo aptitude update
+
 echo "Checking to see if the packages we need exist."
-for package in git motion rclone; do
+for package in git motion rclone aptitude; do
   dpkg -l ${package} || install_packages+=" ${install_packages} "
 done
 
 if [[ -n ${install_packages}]]; then
   echo "  Installing necessary packages."
-  sudo apt-get -y install ${install_packages}
+  sudo aptitude -y install ${install_packages}
 else
   echo "  All necessary packages already installed."
 fi
 
 # Set up the camera
-echo "Checking for Video Device on /dev/vid*"
+echo -n "Checking for Video Device on /dev/vid* ... "
 ls -al /dev/vid* && Echo "Video Device Found."|| sudo modprobe bcm2835-v4l2
 ls -al /dev/vid* && echo "Video Device Found." || echo "Failed to find a video device."
+
+
+# Purging unnecessary packages.
+read -p "Delete some big, unnecessary packages to free up space?"
+
+if [[ ${REPLY} =~ ^[Yy] ]] ; then
+  sudo aptitude purge chromium-browser libreoffice libreoffice-avmedia-backend-gstreamer libreoffice-base\
+     libreoffice-base-core libreoffice-base-drivers libreoffice-calc libreoffice-common  libreoffice-core\
+     libreoffice-draw libreoffice-gtk libreoffice-gtk2 libreoffice-impress libreoffice-java-common\
+     libreoffice-math libreoffice-report-builder-bin libreoffice-style-galaxy libreoffice-systray\
+     libreoffice-writer rpi-chromium-mods libreoffice-sdbc-hsqldb bluej greenfoot wolfram-engine\
+     scratch scratch2 squeak-plugins-scratch
+fi
