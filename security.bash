5918f2b815050841ecaad43c5cb1c73a2ac17918
Alan <alan.congleton@wpengine.com>
2017-10-05 03:19:13 -0500

    cleanup

--- security.bash
+++ security.bash
@@ -3,8 +3,7 @@ localsecout="/home/pi/Documents/secout/"
 rclonesecout="secout:/secout/"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
-if [[ $2 == "start" ]] ; then
+securitystart() {
   # Set up the camera; it likes to disappear on reboot for some reason.
   echo -n "Checking for Video Device on /dev/vid* ... "
   ls -al /dev/vid* && echo "  Video Device Found." || sudo modprobe bcm2835-v4l2
@@ -16,9 +15,18 @@ if [[ $2 == "start" ]] ; then
   else
     sudo motion -bc ${DIR}/conf/motion.conf && echo "  Started Motion Capture" || echo "  Failed to start! 0_0"
   fi
-elif [[ $2 == "stop" ]] ; then
+}
+
+securitystop() {
   sudo killall motion rclone
   sudo chown -R pi. ${localsecout}
   find ${localsecout} -type f -name '*.jpg' | xargs -r jpegoptim
   rclone move ${localsecout} ${rclonesecout}
-fi
+}
+
+
+case $2 in
+  start) securitystart ;;
+  stop) securitystop ;;
+  restart) securitystop ; securitystart ;;
+esac
