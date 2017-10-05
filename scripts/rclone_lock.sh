05260c632673fd7653c280c636b1244d34114760
Sindern <sindern@hotmail.com>
2017-10-05 07:50:17 +0000

    must have changed something?  i forget

--- scripts/rclone_lock.sh
+++ scripts/rclone_lock.sh
@@ -4,8 +4,8 @@ lock=/tmp/rclone_running
 
 if [[ ! -f ${lock}  ]] ; then
    touch ${lock}
-   sudo chown -R pi. /home/pi/Documents/secout/
-   jpegoptim /home/pi/Documents/secout/*
+#   sudo chown -R pi. /home/pi/Documents/secout/
+#   jpegoptim /home/pi/Documents/secout/*
    rclone move /home/pi/Documents/secout/ secout:/secout/
    rm ${lock}
 fi
